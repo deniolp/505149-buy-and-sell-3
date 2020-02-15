f7718c578aea0602de0d7b58c97189b4e444a2df
popov7 <deniolp@gmail.com>
2020-02-15 23:32:03 +0400

    Moving mock data to separate json

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,53 +3,15 @@
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 const {getRandomInt, shuffle} = require(`../../utils`);
+const MOCK_DATA = require(`../../../src/mock-data.json`);
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-  `Продам коллекцию журналов «Огонёк»`,
-  `Отдам в хорошие руки подшивку «Мурзилка»`,
-  `Продам советскую посуду. Почти не разбита`,
-  `Куплю детские санки`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  `Кажется, что это хрупкая вещь.`,
-  `Мой дед не мог её сломать.`,
-  `Кому нужен этот новый телефон, если тут такое...`,
-  `Не пытайтесь торговаться. Цену вещам я знаю.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
 
 const OfferType = {
   offer: `offer`,
   sale: `sale`,
 };
-
 
 const SumRestrict = {
   min: 1000,
@@ -66,11 +28,11 @@ const getPictureFileName = (number) => number > 10 ? `item${number}.jpg` : `item
 const generateOffers = (count) => (
   Array(count).fill({}).map(() => ({
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    description: shuffle(SENTENCES).slice(0, getRandomInt(1, 5)).join(` `),
+    title: MOCK_DATA.titles[getRandomInt(0, MOCK_DATA.titles.length - 1)],
+    description: shuffle(MOCK_DATA.sentences).slice(0, getRandomInt(1, 5)).join(` `),
     sum: getRandomInt(SumRestrict.min, SumRestrict.max),
     picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length - 1)),
+    category: shuffle(MOCK_DATA.categories).slice(0, getRandomInt(1, MOCK_DATA.categories.length - 1)),
   }))
 );
 
