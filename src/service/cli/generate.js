b58f251edf4ed98e704c2c56697607e2f59485c0
popov7 <deniolp@gmail.com>
2020-02-27 18:03:29 +0400

    Making generateOffers more abstract

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -6,6 +6,7 @@ const {getRandomInt, shuffle} = require(`../../utils`);
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
+const TXT_FILES_DIR = `./data/`;
 
 const OfferType = {
   offer: `offer`,
@@ -22,6 +23,21 @@ const PictureRestrict = {
   max: 16,
 };
 
+const makeMockData = async (files) => {
+  let mockData = {};
+  try {
+    for (const file of files) {
+      const fileName = file.split(`.`)[0];
+      const data = await readContent(fileName);
+      mockData[fileName] = data;
+    }
+    return mockData;
+  } catch (error) {
+    console.error(chalk.red(error));
+    return mockData;
+  }
+};
+
 const readContent = async (fileName) => {
   try {
     const content = await fs.readFile(`./data/${fileName}.txt`, `utf8`);
@@ -36,27 +52,25 @@ const readContent = async (fileName) => {
 
 const getPictureFileName = (number) => number > 10 ? `item${number}.jpg` : `item0${number}.jpg`;
 
-const generateOffers = (count, titles, categories, sentences) => (
+const generateOffers = (count, mockData) => (
   Array(count).fill({}).map(() => ({
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
-    title: titles[getRandomInt(0, titles.length - 1)],
-    description: shuffle(sentences).slice(0, getRandomInt(1, 5)).join(` `),
+    title: mockData.titles[getRandomInt(0, mockData.titles.length - 1)],
+    description: shuffle(mockData.sentences).slice(0, getRandomInt(1, 5)).join(` `),
     sum: getRandomInt(SumRestrict.min, SumRestrict.max),
     picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
-    category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
+    category: shuffle(mockData.categories).slice(0, getRandomInt(1, mockData.categories.length - 1)),
   }))
 );
 
 module.exports = {
   name: `--generate`,
   async run(args) {
-    const sentences = await readContent(`sentences`);
-    const titles = await readContent(`titles`);
-    const categories = await readContent(`categories`);
-
+    const files = await fs.readdir(TXT_FILES_DIR);
+    const mockData = await makeMockData(files);
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences), null, 2);
+    const content = JSON.stringify(generateOffers(countOffer, mockData), null, 2);
 
     try {
       await fs.writeFile(FILE_NAME, content);
