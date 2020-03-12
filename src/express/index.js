7bce043b46d6db118679c63cae0112ff6bab6172
popov7 <deniolp@gmail.com>
2020-03-12 17:58:40 +0500

    Creating first template for page /offers/category

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const myRoutes = require(`./routes/my`);
 const offersRoutes = require(`./routes/offers`);
 
 const app = express();
-const port = 8080;
+const port = 8081;
 app.listen(port);
+
+app.use(express.static(path.join(__dirname, `markup`)));
+
+app.set(`views`, path.join(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
