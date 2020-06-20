8d39bf2af2dea6684345f4f23f825c80b52d9b5c
popov7 <deniolp@gmail.com>
2020-06-20 12:21:12 +0300

    Correcting mistake in CategoryServise.findAll

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -5,7 +5,6 @@ const {Router} = require(`express`);
 const category = require(`../api/category`);
 const offer = require(`../api/offer`);
 const search = require(`../api/search`);
-
 const getMockData = require(`../lib/get-mock-data`);
 
 const {
@@ -21,8 +20,8 @@ const app = new Router();
   const mockData = await getMockData();
 
   category(app, new CategoryService(mockData));
-  search(app, new SearchService(mockData));
   offer(app, new OfferService(mockData), new CommentService());
+  search(app, new SearchService(mockData));
 })();
 
 module.exports = app;
