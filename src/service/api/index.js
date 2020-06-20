a9254aefaacd6a164baeb9e575ffb66cc17b4ca9
popov7 <deniolp@gmail.com>
2020-06-20 11:04:54 +0300

    Implementing /api/search

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -4,13 +4,13 @@ const {Router} = require(`express`);
 
 const category = require(`../api/category`);
 const offer = require(`../api/offer`);
-// const search = require(`../api/search`);
+const search = require(`../api/search`);
 
 const getMockData = require(`../lib/get-mock-data`);
 
 const {
   CategoryService,
-  // SearchService,
+  SearchService,
   OfferService,
   CommentService,
 } = require(`../data-service`);
@@ -21,7 +21,7 @@ const app = new Router();
   const mockData = await getMockData();
 
   category(app, new CategoryService(mockData));
-  // search(app, new SearchService(mockData));
+  search(app, new SearchService(mockData));
   offer(app, new OfferService(mockData), new CommentService());
 })();
 
