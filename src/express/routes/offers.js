ad3a19de7fad37463db7c137ebaa9b660c28f8ff
popov7 <deniolp@gmail.com>
2020-07-01 23:43:02 +0300

    Filling ticket-edit.pug template

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -1,11 +1,22 @@
 'use strict';
 
 const {Router} = require(`express`);
+
+const getOffers = require(`../api/offers`);
+
 const offersRouter = new Router();
 
 offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`, {}));
 offersRouter.get(`/category`, (req, res) => res.render(`category`, {}));
 offersRouter.get(`/:id`, (req, res) => res.render(`ticket`, {}));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`, {}));
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const offers = await getOffers();
+  const offer = await offers.find((item) => item.id === id);
+
+  res.render(`ticket-edit`, {
+    offer,
+  });
+});
 
 module.exports = offersRouter;
