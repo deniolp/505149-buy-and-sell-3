5985306d29323808e37050cfb264da015a7ef5ce
popov7 <deniolp@gmail.com>
2020-07-02 18:49:19 +0300

    Correcting /my

--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -3,20 +3,14 @@
 const {Router} = require(`express`);
 
 const getOffers = require(`../api/offers`);
-const getCategories = require(`../api/categories`);
-const {getSortedByCommentAmount} = require(`../../utils`);
-
 
 const myRouter = new Router();
 
 myRouter.get(`/`, async (req, res) => {
   const offers = await getOffers();
-  const categories = await getCategories();
 
-  res.render(`main`, {
+  res.render(`my-tickets`, {
     offers,
-    mostDiscussed: getSortedByCommentAmount(offers),
-    categories,
   });
 });
 myRouter.get(`/comments`, async (req, res) => {
