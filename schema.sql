ce883bcdfc6db43b9675c755db47d5187994832c
popov7 <deniolp@gmail.com>
2020-07-14 14:57:08 +0300

    Writing first queries for creating tables (offers and users)

--- schema.sql
+++ schema.sql
@@ -18,3 +18,33 @@ CREATE DATABASE byu_and_sell
     CONNECTION LIMIT = -1;
 
 GRANT ALL ON DATABASE byu_and_sell TO byu_sell;
+
+CREATE TYPE offer_type AS ENUM ('buy', 'offer');
+
+CREATE TABLE offers
+(
+    id bigserial NOT NULL,
+    type offer_type NOT NULL,
+    title character varying(100) NOT NULL,
+    description character varying(1000) NOT NULL,
+    sum numeric NOT NULL,
+    picture character varying(500),
+    created_date DATE NOT NULL,
+    user_id bigint NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT offers_users FOREIGN KEY (user_id)
+        REFERENCES users (id) MATCH FULL
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE users
+(
+    id bigserial NOT NULL,
+    first_name character varying(50) NOT NULL,
+    last_name character varying(50) NOT NULL,
+    email character varying(50) NOT NULL,
+    password character varying(50) NOT NULL,
+    avatar character varying(50),
+    PRIMARY KEY (id)
+);
