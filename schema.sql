2f8aae588e1054dc79012aa4b3b397a9225d0f39
popov7 <deniolp@gmail.com>
2020-07-14 17:03:00 +0300

    Writing query for creating table comments and linking it with offers and users

--- schema.sql
+++ schema.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS offers;
 
 CREATE ROLE buy_sell WITH
-	LOGIN
-	NOSUPERUSER
-	CREATEDB
-	NOCREATEROLE
-	INHERIT
-	NOREPLICATION
-	CONNECTION LIMIT -1
-	PASSWORD '';
+    LOGIN
+    NOSUPERUSER
+    CREATEDB
+    NOCREATEROLE
+    INHERIT
+    NOREPLICATION
+    CONNECTION LIMIT -1
+    PASSWORD '';
 
 CREATE DATABASE buy_and_sell
     WITH
@@ -22,18 +22,18 @@ CREATE DATABASE buy_and_sell
     LC_CTYPE = 'C'
     CONNECTION LIMIT = -1;
 
-CREATE TYPE offer_type AS ENUM ('buy', 'offer');
-
 CREATE TABLE users
 (
     id bigserial NOT NULL,
     first_name character varying(50) NOT NULL,
     last_name character varying(50) NOT NULL,
-    email character varying(50) NOT NULL,
+    email character varying(50) UNIQUE NOT NULL,
     password character varying(50) NOT NULL,
     avatar character varying(50),
     PRIMARY KEY (id)
 );
+
+CREATE TYPE offer_type AS ENUM ('buy', 'offer');
 
 CREATE TABLE offers
 (
@@ -48,6 +48,23 @@ CREATE TABLE offers
     PRIMARY KEY (id),
     CONSTRAINT offers_users FOREIGN KEY (user_id)
         REFERENCES users (id) MATCH FULL
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE comments
+(
+    id bigserial PRIMARY KEY,
+    text character varying(300) NOT NULL,
+    created_date DATE NOT NULL,
+    user_id bigint NOT NULL,
+    offer_id bigint NOT NULL,
+    CONSTRAINT comments_users FOREIGN KEY (user_id)
+        REFERENCES users (id) MATCH FULL
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT comments_offers FOREIGN KEY (offer_id)
+        REFERENCES offers (id) MATCH FULL
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
