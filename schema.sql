e6a8c9081856a60702a5322480ab505abc465596
popov7 <deniolp@gmail.com>
2020-07-14 14:08:52 +0300

    Writing query for creating DATABASE

--- schema.sql
+++ schema.sql
@@ -7,3 +7,14 @@ CREATE ROLE byu_sell WITH
 	NOREPLICATION
 	CONNECTION LIMIT -1
 	PASSWORD '';
+
+CREATE DATABASE byu_and_sell
+    WITH
+    OWNER = byu_sell
+    TEMPLATE = template0
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    CONNECTION LIMIT = -1;
+
+GRANT ALL ON DATABASE byu_and_sell TO byu_sell;
