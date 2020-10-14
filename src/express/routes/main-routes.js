002026b40799eb98500970ddefa9fb299458ea87
deniolp <deniolp@gmail.com>
2020-10-15 00:15:56 +0300

    Adding rendering empty search page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -19,14 +19,19 @@ mainRouter.get(`/`, async (req, res) => {
     categories,
   });
 });
-mainRouter.get(`/register`, (req, res) => res.render(`sign-up`, {}));
+mainRouter.get(`/register`, (req, res) => res.render(`register`, {}));
 mainRouter.get(`/login`, (req, res) => res.render(`login`, {}));
 mainRouter.get(`/search`, async (req, res) => {
-  const encodedURI = encodeURI(req.query.search);
-  const offers = await getSearchResults(encodedURI);
   const allOffers = await getOffers();
   const eightOffers = allOffers.slice(0, 8);
   const moreOffersQty = allOffers.length >= 8 ? (allOffers.length) - 8 : null;
+
+  if (!req.query.search) {
+    res.render(`search-empty`, {eightOffers, moreOffersQty});
+    return;
+  }
+  const encodedURI = encodeURI(req.query.search);
+  const offers = await getSearchResults(encodedURI);
 
   res.render(`search-result`, {offers, eightOffers, moreOffersQty});
 });
