e6ef1d13a02bca04208f2b0d0a957df7f319025b
deniolp <deniolp@gmail.com>
2021-01-05 10:10:38 +0300

    Adding check are we vave all env vars

--- src/service/database/index.js
+++ src/service/database/index.js
@@ -9,6 +9,12 @@ const {createCommentModel, createCommentLinks} = require(`./models/comment`);
 const {getLogger} = require(`../lib/logger`);
 const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT} = require(`../../../config`);
 
+const somethingIsNotDefined = [DB_NAME, DB_USER, DB_PASSWORD, DB_HOST].some((it) => it === undefined);
+
+if (somethingIsNotDefined) {
+  throw new Error(`One or more environmental variables are not defined`);
+}
+
 const logger = getLogger({
   name: `db-server`,
 });
@@ -17,6 +23,12 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: DB_DIALECT,
   logging: (msg) => logger.debug(msg),
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 10000,
+    idle: 10000
+  }
 });
 
 const User = createUserModel(sequelize);
