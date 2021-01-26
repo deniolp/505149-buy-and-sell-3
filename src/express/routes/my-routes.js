f48b4f3a0b682e95b4be8a972e2b005f53c5a829
deniolp <deniolp@gmail.com>
2021-01-27 00:16:59 +0300

    Implementing pagination on page with comments

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -5,10 +5,9 @@ const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
 
 const OFFERS_PER_PAGE = 8;
+const OFFERS_PER_PAGE_COMMENTS = 3;
 
 const myRouter = new Router();
-
-let myOffers = null;
 
 myRouter.get(`/`, async (req, res) => {
   let {page = 1} = req.query;
@@ -28,18 +27,28 @@ myRouter.get(`/`, async (req, res) => {
 });
 
 myRouter.get(`/comments`, async (req, res) => {
-  if (!myOffers) {
-    myOffers = (await api.getOffers()).slice(0, 3);
-  }
-  const offersId = myOffers.map((it) => it.id);
+  let {page = 1} = req.query;
+  page = +page;
+
+  const limit = OFFERS_PER_PAGE_COMMENTS;
+  const offset = (page - 1) * OFFERS_PER_PAGE_COMMENTS;
+  const {count, offers} = await api.getOffers({limit, offset});
+  const totalPages = Math.ceil(count / OFFERS_PER_PAGE_COMMENTS);
+
+  const offersId = offers.map((it) => it.id);
   await Promise.all(offersId.map((id) => api.getComments(id)))
       .then((results) => {
         for (const [index, array] of results.entries()) {
-          myOffers[index].comments = array;
+          offers[index].comments = array;
         }
       });
 
-  res.render(`comments`, {myOffers});
+  res.render(`comments`, {
+    title: `Мои комментарии`,
+    offers,
+    page,
+    totalPages,
+  });
 });
 
 module.exports = myRouter;
