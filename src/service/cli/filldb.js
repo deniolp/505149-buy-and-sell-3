7022c5153dd30000d33f5bbb0ed35cd9b4b92b08
deniolp <deniolp@gmail.com>
2021-08-05 11:30:14 +0300

    Refactoring /cli filldb

--- src/service/cli/filldb.js
+++ src/service/cli/filldb.js
@@ -2,13 +2,18 @@
 
 const fs = require(`fs`).promises;
 
-const {sequelize, models} = require(`../database`);
+const sequelize = require(`../lib/sequelize`);
 const {getLogger} = require(`../lib/logger`);
-const {getRandomInt, shuffle, OfferType, SumRestrict, PictureRestrict, getPictureFileName, makeMockData} = require(`../../utils`);
+const initDatabase = require(`../lib/init-db`);
+const {getRandomInt, shuffle, OfferType, SumRestrict, PictureRestrict, getPictureFileName, makeMockData, readContent} = require(`../../utils`);
 
-const {TXT_FILES_DIR, ExitCode} = require(`../../constants`);
-const DEFAULT_COUNT = 5;
-const MAX_COMMENTS = 50;
+const {
+  MAX_DATA_COUNT,
+  TXT_FILES_DIR,
+  ExitCode,
+  DEFAULT_COUNT,
+  MAX_COMMENTS
+} = require(`../../constants`);
 
 const users = [
   {
@@ -28,23 +33,15 @@ const users = [
 ];
 
 const logger = getLogger({
-  name: `api-server`,
+  name: `api-filldb`,
 });
 
-const generateCategories = (categories) => (
-  categories.map((item) => ({
-    title: item,
-    picture: `picture.png`,
-  }))
-);
-
-const generateComments = (comments, countOffer) => (
-  Array(MAX_COMMENTS).fill({}).map(() => ({
+const generateComments = (count, comments) => (
+  Array(count).fill({}).map(() => ({
     text: shuffle(comments)
       .slice(0, getRandomInt(1, 3))
       .join(` `),
-    [`user_id`]: getRandomInt(1, 2),
-    [`offer_id`]: getRandomInt(1, countOffer),
+    user: users[getRandomInt(0, users.length - 1)].email,
   }))
 );
 
@@ -55,79 +52,37 @@ const generateOffers = (count, mockData) => (
     description: shuffle(mockData.sentences).slice(0, getRandomInt(1, 5)).join(` `),
     sum: getRandomInt(SumRestrict.min, SumRestrict.max),
     picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
-    category: shuffle(mockData.categories).slice(0, getRandomInt(1, mockData.categories.length - 1)),
-    [`user_id`]: getRandomInt(1, 2),
+    categories: shuffle(mockData.categories).slice(0, getRandomInt(1, mockData.categories.length - 1)),
+    comments: generateComments(getRandomInt(1, MAX_COMMENTS), mockData.comments),
+    user: users[getRandomInt(0, users.length - 1)].email,
   }))
 );
-
-const fillDataBase = async (categories, comments, offers) => {
-  try {
-    await Promise.all([
-      models.User.bulkCreate(users),
-      models.Category.bulkCreate(categories)
-    ]);
-
-    await models.Offer.bulkCreate(offers);
-    await models.Comment.bulkCreate(comments);
-
-    const rawCategories = await models.Category.findAll({raw: true});
-
-    for (const offer of offers) {
-      const categoriesIds = rawCategories.reduce((acc, item) => {
-        if (offer.categories.filter((cat) => cat === item.title).length) {
-          acc.push(item.id);
-        }
-        return acc;
-      }, []);
-
-      const offerCategories = await models.Category.findAll({
-        where: {
-          id: {
-            [sequelize.Sequelize.Op.or]: categoriesIds,
-          },
-        }
-      });
-
-      const rawOffer = await models.Offer.findOne({
-        where: {
-          title: offer.title,
-          sum: offer.sum,
-        }
-      });
-      await rawOffer.addCategories(offerCategories);
-    }
-  } catch (error) {
-    logger.error(`An error occured while filling database: ${error.message}`);
-  }
-
-};
 
 module.exports = {
   name: `--filldb`,
   async run(args) {
-    let count;
     try {
-      const files = await fs.readdir(TXT_FILES_DIR);
-      if (args) {
-        [count] = args;
-      }
-      const countOfOffers = Number.parseInt(count, 10) || DEFAULT_COUNT;
-      const mockData = await makeMockData(files);
-      const categories = generateCategories(mockData.categories);
-      const comments = generateComments(mockData.comments, countOfOffers);
-      const offers = generateOffers(countOfOffers, mockData);
-
       logger.info(`Trying to connect to database...`);
-      const result = await sequelize.sync({force: true});
-      logger.info(`Successfully created ${result.config.database} database`);
+      await sequelize.authenticate();
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(1);
+    }
+    logger.info(`Connection to database established`);
 
-      await fillDataBase(categories, comments, offers);
-      logger.info(`Successfully filled ${result.config.database} database`);
-      sequelize.close();
 
-    } catch (error) {
-      logger.error(`An error occured: ${error.message}`);
+    const [count] = args;
+    if (count >= MAX_DATA_COUNT) {
+      logger.error(`Не больше 1000 объявлений`);
       process.exit(ExitCode.error);
     }
+
+    const countOffers = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const files = await fs.readdir(TXT_FILES_DIR);
+    const mockData = await makeMockData(files);
+    const categories = await readContent(`categories`);
+    const offers = generateOffers(countOffers, mockData);
+
+    return initDatabase(sequelize, {offers, categories, users});
   }
 };
