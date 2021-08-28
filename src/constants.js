d0207f465af1d88f981d71fb4cd4e1d4ddaf2f0f
deniolp <deniolp@gmail.com>
2021-08-28 11:48:07 +0300

    Changing relative path to absolute

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require(`path`);
+
 module.exports.DEFAULT_COMMAND = `--help`;
 module.exports.MAX_DATA_COUNT = 1000;
 module.exports.USER_ARGV_INDEX = 2;
@@ -10,6 +12,7 @@ module.exports.TXT_FILES_DIR = `./data/`;
 module.exports.FILE_NAME = `mocks.json`;
 module.exports.PUBLIC_DIR = `files`;
 module.exports.UPLOAD_DIR = `upload`;
+module.exports.MULTER_UPLOAD_DIR = path.resolve(__dirname, `express/upload/img/`);
 module.exports.TIMEOUT = 1000;
 module.exports.OFFERS_PER_PAGE = 8;
 module.exports.SALT_ROUNDS = 10;
