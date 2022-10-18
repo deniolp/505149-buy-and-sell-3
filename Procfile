807abac554d8418276808d472b3a5bc41b26797b
Liam Connor <deniolp@gmail.com>
2022-10-18 17:37:38 +0200

    Adding envs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run run::prod
+web: npm run dev
--- config.js
+++ config.js
@@ -4,10 +4,10 @@ require(`dotenv`).config();
 
 module.exports = {
   APP_URL: process.env.APP_URL || `http://localhost`,
-  APP_PORT: process.env.APP_PORT || 8080,
+  APP_PORT: process.env.PORT || 8080,
   API_PORT: process.env.API_PORT || 3000,
   DB_PORT: process.env.DB_PORT || 5432,
-  DB_NAME: process.env.DB_NAME || `buy_and_sell`,
+  DB_NAME: process.env.DATABASE_URL || `buy_and_sell`,
   TEST_DB_NAME: process.env.TEST_DB_NAME || `test_buy_and_sell`,
   DB_USER: process.env.DB_USER || `buy_sell`,
   DB_HOST: process.env.DB_HOST || `localhost`,
